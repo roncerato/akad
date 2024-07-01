b07e0780167ea82878d36b3b60bd1a3620dbcd6b
Ron <nazarmuhamedow9731@gmail.com>
2024-07-02 00:16:29 +0500

    Changed className & name function

--- src/components/template/texts/t3.tsx
+++ src/components/template/texts/t3.tsx
@@ -2,9 +2,9 @@ interface IProps {
     children: React.ReactNode
 }
 
-export default function T2({ children }: IProps) {
+export default function T3({ children }: IProps) {
     return (
-        <p className="t-2">
+        <p className="t-3">
             {children}
         </p>
     )
