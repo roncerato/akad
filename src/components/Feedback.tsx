5ec6895ae84302e73a5ed8c043a90db6d6316c27
Ron <nazarmuhamedow9731@gmail.com>
2024-07-04 03:11:53 +0500

    Created feedback component

---
+++ src/components/Feedback.tsx
@@ -0,0 +1,39 @@
+"use client"
+
+import Button from "./template/Button";
+import H3 from "./template/headings/h3";
+import H4 from "./template/headings/h4";
+import T1 from "./template/texts/t1";
+
+export default function Feedback() {
+    return (
+        <section className="feedback-section">
+            <div className="feedback-container container">
+                <div className="feedback-left">
+                    <H3>
+                        YOU THINK WE'RE COOL ? LET'S WORK TOGETHER
+                    </H3>
+                    <Button>
+                        get in touch
+                    </Button>
+                </div>
+                <div className="feedback-right">
+                    <H4>
+                        STAY INFORMED WITH OUR NEWSLETTER
+                    </H4>
+                    <T1>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    </T1>
+                    <form action="" onSubmit={(event) => {
+                        event.preventDefault()
+                    }}>
+                        <input type="email" name="" id="" placeholder="YOUR EMAIL" />
+                        <Button isColored={true}>
+                            SEND
+                        </Button>
+                    </form>
+                </div>
+            </div>
+        </section>
+    )
+}
