f3e2edae4f8aa05a27da00010ef3a79353b7314a
Ron <nazarmuhamedow9731@gmail.com>
2024-07-04 03:55:34 +0500

    Added footer component in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { Open_Sans } from "next/font/google";
 import "@/styles/global.scss"
 import Header from "@/components/Header";
 import "animate.css"
+import Footer from "@/components/Footer";
 
 const openSans = Open_Sans({ subsets: ["latin", "cyrillic"] });
 
@@ -16,7 +17,7 @@ export default function RootLayout({
             <body className={openSans.className}>
                 <Header />
                 {children}
-
+                <Footer />
             </body>
         </html>
     );
