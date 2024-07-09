a2ec229dd248586279a57c5692f63cf5cd0263bf
Ron <nazarmuhamedow9731@gmail.com>
2024-07-09 17:27:52 +0500

    Created benefits list data

---
+++ src/constants/benefits.ts
@@ -0,0 +1,45 @@
+interface IBenefit {
+    id: string
+    imgSrc: string
+    title: string
+    description: string
+}
+
+export const benefits: IBenefit[] = [
+    {
+        id: "1",
+        imgSrc: "/jpg/phone.png",
+        title: "FULLY RESPONSIVE",
+        description: "Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In eleifend suscipit enim, eu commodo neque molestie vitae."
+    },
+    {
+        id: "2",
+        imgSrc: "/jpg/unlimited.png",
+        title: "UNLIMITED OPTIONS",
+        description: "Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In eleifend suscipit enim, eu commodo neque molestie vitae."
+    },
+    {
+        id: "3",
+        imgSrc: "/jpg/wordpress.png",
+        title: "WORDPRESS",
+        description: "Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In eleifend suscipit enim, eu commodo neque molestie vitae."
+    },
+    {
+        id: "4",
+        imgSrc: "/jpg/shopCart.png",
+        title: "e-commerce",
+        description: "Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In eleifend suscipit enim, eu commodo neque molestie vitae."
+    },
+    {
+        id: "5",
+        imgSrc: "/jpg/lines.png",
+        title: "CUSTOMIZABLE DESIGN",
+        description: "Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In eleifend suscipit enim, eu commodo neque molestie vitae."
+    },
+    {
+        id: "6",
+        imgSrc: "/jpg/settings.png",
+        title: "SUPPORT",
+        description: "Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In eleifend suscipit enim, eu commodo neque molestie vitae."
+    },
+]
