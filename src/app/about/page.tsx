c54ab0d162c392b90ad475910043ab88c228b560
Ron <nazarmuhamedow9731@gmail.com>
2024-07-09 17:29:40 +0500

    Added description & benefits component in about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,4 +1,6 @@
 import Newsletter from "@/components/Newsletter";
+import Benefits from "@/components/about/Benefits";
+import Description from "@/components/about/Description";
 import HeroTemplate from "@/components/template/Hero";
 import "@/styles/about/style.scss"
 
@@ -6,6 +8,8 @@ export default function About() {
     return (
         <main>
             <HeroTemplate classN={"about"} title={"about us"} breadcrumbs={"about"} />
+            <Description />
+            <Benefits />
             <Newsletter />
         </main>
     )
