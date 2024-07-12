87f6866dd2d8fa6d269b4ebeaa5a6609ff7ccfd5
Ron <nazarmuhamedow9731@gmail.com>
2024-07-13 03:02:52 +0500

    Imported main & sidebar component in blog page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,4 +1,6 @@
 import Newsletter from "@/components/Newsletter";
+import Main from "@/components/blog/Main";
+import Sidebar from "@/components/blog/Sidebar";
 import HeroTemplate from "@/components/template/Hero";
 import "@/styles/blog/style.scss"
 
@@ -6,6 +8,12 @@ export default function Blog() {
     return (
         <main>
             <HeroTemplate classN={"blog"} title={"blog posts"} breadcrumbs={"blog"} />
+            <div className="blog-section">
+                <div className="blog-container container">
+                    <Main />
+                    <Sidebar />
+                </div>
+            </div>
             <Newsletter />
         </main>
     )
