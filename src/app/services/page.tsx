b79e240487bbd37630f85c8f657f0deab741ee35
Ron <nazarmuhamedow9731@gmail.com>
2024-07-13 22:31:38 +0500

    Imported wwd component in services page

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,4 +1,5 @@
 import Newsletter from "@/components/Newsletter";
+import WhatWeDo from "@/components/services/WhatWeDo";
 import HeroTemplate from "@/components/template/Hero";
 import "@/styles/services/style.scss"
 
@@ -6,6 +7,7 @@ export default function Services() {
     return (
         <main>
             <HeroTemplate classN={"services"} title={"our services"} breadcrumbs={"services"} />
+            <WhatWeDo />
             <Newsletter />
         </main>
     )
