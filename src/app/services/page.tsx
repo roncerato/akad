49961b5a8eda4c7dd05fa1533f9403f003df562d
Ron <nazarmuhamedow9731@gmail.com>
2024-07-15 01:29:25 +0500

    Imported benefits component in services page

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,4 +1,5 @@
 import Newsletter from "@/components/Newsletter";
+import Benefits from "@/components/services/Benefits";
 import WhatWeDo from "@/components/services/WhatWeDo";
 import HeroTemplate from "@/components/template/Hero";
 import "@/styles/services/style.scss"
@@ -8,6 +9,7 @@ export default function Services() {
         <main>
             <HeroTemplate classN={"services"} title={"our services"} breadcrumbs={"services"} />
             <WhatWeDo />
+            <Benefits />
             <Newsletter />
         </main>
     )
