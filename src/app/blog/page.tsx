9bf27456b455357cb7b1618a033b9df4c9eccb50
Ron <nazarmuhamedow9731@gmail.com>
2024-08-01 19:42:13 +0500

    Refactored the blog component, moved imports, created BlogSection, and added useState for data from URL.

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -2,18 +2,16 @@ import Newsletter from "@/components/Newsletter";
 import Main from "@/components/blog/Main";
 import Sidebar from "@/components/blog/Sidebar";
 import HeroTemplate from "@/components/template/Hero";
+import BlogSection from "@/components/blog/BlogSection";
 import "@/styles/blog/style.scss"
 
+
 export default function Blog() {
+
     return (
         <main>
             <HeroTemplate classN={"blog"} title={"blog posts"} breadcrumbs={"blog"} />
-            <div className="blog-section">
-                <div className="blog-container container">
-                    <Main />
-                    <Sidebar />
-                </div>
-            </div>
+            <BlogSection/>
             <Newsletter />
         </main>
     )
