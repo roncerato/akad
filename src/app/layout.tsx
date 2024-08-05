0da5e58ab336f2b2b7e39f758aa7104978e87481
Ron <nazarmuhamedow9731@gmail.com>
2024-08-06 02:09:21 +0500

    Added metadata in project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,9 +3,20 @@ import "@/styles/global.scss"
 import Header from "@/components/Header";
 import "animate.css"
 import Footer from "@/components/Footer";
+import { Metadata } from "next";
+import { headers } from "next/headers";
 
 const openSans = Open_Sans({ subsets: ["latin", "cyrillic"] });
 
+
+export async function generateMetadata(): Promise<Metadata>{
+    const head = headers();
+    const pathname = head.get("next-url");
+    
+    return {
+        title: `${pathname?.slice(22).charAt(0).toUpperCase()}${pathname?.slice(23)} | AKAD.`
+    }
+}
 
 export default function RootLayout({
     children,
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,17 @@ import Advantages from "@/components/home/Advantages";
 import Hero from "@/components/home/Hero";
 import Portfolio from "@/components/home/Portfolio";
 import Newsletter from "@/components/Newsletter";
+import { Metadata } from "next/types";
 
+export async function generateMetadata(): Promise<Metadata>{
+  
+    return {
+        title: `AKAD.`
+    }
+}
 
 export default function Home() {
+    
     return (
         <main>
             <Hero />
