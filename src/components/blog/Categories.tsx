1c0e3ddd770180694ede34a77a14122dd5a5980c
Ron <nazarmuhamedow9731@gmail.com>
2024-08-23 00:08:52 +0500

    Added preloading in categories component

--- src/components/blog/Categories.tsx
+++ src/components/blog/Categories.tsx
@@ -1,6 +1,13 @@
 import { useEffect, useState } from "react";
 import H6 from "../template/headings/h6";
 import T2 from "../template/texts/t2";
+import { Flow_Rounded } from "next/font/google"
+
+const flowRounded = Flow_Rounded({
+    weight: "400",
+    preload: true,
+    subsets: ["latin"]
+})
 
 interface CategoriesProps {
     setUrl: React.Dispatch<React.SetStateAction<string>>;
@@ -37,12 +44,34 @@ const Categories: React.FC<CategoriesProps> = ({ setUrl, values, getRandomVal })
         <div className="sidebar-categories">
             <H6>Categories</H6>
             <ul className="sidebar-categories-list">
-                {randomValues.map((item, index) => (
-                    <li className="siddebar-categories-item" key={Math.random().toString(36).slice(2)} onClick={()=>setUrl(`https://dummyjson.com/posts/tag/${item}`)}>
-                        <T2>{item}</T2>
-                        <T2>{valuesQuantity[index]}</T2>
-                    </li>
-                ))}
+                {
+                    valuesQuantity
+                        ? randomValues.map((item, index) => (
+                            <li className="siddebar-categories-item" key={Math.random().toString(36).slice(2)} onClick={() => setUrl(`https://dummyjson.com/posts/tag/${item}`)}>
+                                <T2>
+                                    <span className={flowRounded.className}>
+                                        {item}
+
+                                    </span>
+                                </T2>
+                                <T2>{valuesQuantity[index]}</T2>
+                            </li>
+                        )) :
+                        [0, 0, 0, 0, 0].map(() => (
+                            <li key={Math.random().toString(36).slice(2)} style={{ display: "flex", flexDirection: "row", justifyContent: "space-between" }}>
+                                <T2>
+                                    <span className={`${flowRounded.className} loading-text`}>
+                                       Summer
+                                    </span>
+                                </T2>
+                                <T2>
+                                    <span className={`${flowRounded.className} loading-text`}>
+                                        9
+                                    </span>
+                                </T2>
+                            </li>
+                        ))
+                }
             </ul>
         </div>
     );
