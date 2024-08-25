5765663d05d197d90d991d72324da57884d2aeb4
Ron <nazarmuhamedow9731@gmail.com>
2024-08-26 04:50:50 +0500

    Fix issue with apostrophe encoding in HTML contenе

--- src/components/blog/post/PostLoading.tsx
+++ src/components/blog/post/PostLoading.tsx
@@ -37,7 +37,7 @@ export default function PostLoading() {
 
                 <T1>
                     <span className={`${flowRounded.className} loading-text`}>
-                        His mother had always taught him not to ever think of himself as better than others. He'd tried to live by this motto. He never looked down on those who were less fortunate or who had less money than him. But the stupidity of the group of people he was talking to made him change his mind.
+                        His mother had always taught him not to ever think of himself as better than others. He&apos;d tried to live by this motto. He never looked down on those who were less fortunate or who had less money than him. But the stupidity of the group of people he was talking to made him change his mind.
                     </span>
                 </T1>
             </div>
