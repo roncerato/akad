3a4ccb4e64199dcf1115eee72355a83b64cdce64
Ron <nazarmuhamedow9731@gmail.com>
2024-08-26 02:49:03 +0500

    Added loading page & css

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+import "@/styles/loading.scss"
+
+export default function Loading(): JSX.Element {
+    return (
+        <div className="loader-page-wrap">
+            <div className="loader-page"></div>
+        </div>
+    )
+}
