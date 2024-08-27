849bd5794b8b53969c15643b17b46e2bc4e3785b
Ron <nazarmuhamedow9731@gmail.com>
2024-08-28 00:02:20 +0500

    Replaced PNG images with WebP in the Clients component for improved loading performance

--- src/constants/clients.ts
+++ src/constants/clients.ts
@@ -8,32 +8,32 @@ export const clients: IClients[] = [
     {
         name: "Archery",
         id: "1",
-        img: "/jpg/clients/1.png"
+        img: "/webp/about/clients/1.webp"
     },
     {
         name: "Pacifiana",
         id: "2",
-        img: "/jpg/clients/2.png"
+        img: "/webp/about/clients/2.webp"
     },
     {
         name: "Merchant",
         id: "3",
-        img: "/jpg/clients/3.png"
+        img: "/webp/about/clients/3.webp"
     },
     {
         name: "Atelier de l'Armee",
         id: "4",
-        img: "/jpg/clients/4.png"
+        img: "/webp/about/clients/4.webp"
     },
     {
         name: "STEAK HOUSE",
         id: "5",
-        img: "/jpg/clients/5.png"
+        img: "/webp/about/clients/5.webp"
     },
     {
         name: "WATSON",
         id: "6",
-        img: "/jpg/clients/6.png"
+        img: "/webp/about/clients/6.webp"
     },
 
 ]
