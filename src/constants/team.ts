f038e2ad56e808d40691988b8e1f12f25e4f5473
Ron <nazarmuhamedow9731@gmail.com>
2024-08-28 00:01:41 +0500

    Replaced PNG images with WebP in the Team component for improved loading performance

--- src/constants/team.ts
+++ src/constants/team.ts
@@ -17,7 +17,7 @@ export const team: ITeam[] = [
         "lastName": "Williams",
         "email": "michael.williams@x.dummyjson.com",
         "phone": "+49 258-627-6644",
-        "image": "/jpg/team/1.png",
+        "image": "/webp/about/team/1.webp",
         "company": {
             "title": "Team Leader",
         },
@@ -28,7 +28,7 @@ export const team: ITeam[] = [
         "lastName": "Johnson",
         "email": "emily.johnson@x.dummyjson.com",
         "phone": "+81 965-431-3024",
-        "image": "/jpg/team/2.png",
+        "image": "/webp/about/team/2.webp",
         "company": {
             "title": "Designer",
         },
@@ -39,7 +39,7 @@ export const team: ITeam[] = [
         "lastName": "Davis",
         "email": "james.davis@x.dummyjson.com",
         "phone": "+49 614-958-9364",
-        "image": "/jpg/team/3.png",
+        "image": "/webp/about/team/3.webp",
         "company": {
             "title": "Tester",
         },
@@ -50,7 +50,7 @@ export const team: ITeam[] = [
         "lastName": "Brown",
         "email": "sophia.brown@x.dummyjson.com",
         "phone": "+81 210-652-2785",
-        "image": "/jpg/team/4.png",
+        "image": "/webp/about/team/4.webp",
         "company": {
             "title": "Backend Developer",
         },
@@ -61,7 +61,7 @@ export const team: ITeam[] = [
         "lastName": "Jones",
         "email": "alexander.jones@x.dummyjson.com",
         "phone": "+61 260-824-4986",
-        "image": "/jpg/team/5.png",
+        "image": "/webp/about/team/5.webp",
         "company": {
             "title": "Frontend Developer",
         },
@@ -72,7 +72,7 @@ export const team: ITeam[] = [
         "lastName": "Martinez",
         "email": "ethan.martinez@x.dummyjson.com",
         "phone": "+92 933-608-5081",
-        "image": "/jpg/team/6.png",
+        "image": "/webp/about/team/6.webp",
         "company": {
             "title": "UX/UI Designer",
         },
@@ -83,7 +83,7 @@ export const team: ITeam[] = [
         "lastName": "Miller",
         "email": "emma.miller@x.dummyjson.com",
         "phone": "+91 759-776-1614",
-        "image": "/jpg/team/7.png",
+        "image": "/webp/about/team/7.webp",
         "company": {
             "title": "Quality Assurance Engineer",
         },
@@ -94,7 +94,7 @@ export const team: ITeam[] = [
         "lastName": "Garcia",
         "email": "liam.garcia@x.dummyjson.com",
         "phone": "+92 870-217-6201",
-        "image": "/jpg/team/8.png",
+        "image": "/webp/about/team/8.webp",
         "company": {
             "title": "Full-Stack Developer",
         },
